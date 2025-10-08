7fb1406eb8d578d4d82d2a185c2e11e906a992fe
Nicojerson <nicojersonsky1234@gmail.com>
2025-10-08 13:08:29 -0300

    Lo Hice!

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import {
   seleccionarProducto,
   seleccionarSabores,
 } from "./dataEntry.js";
+import fs from "fs"
 
 // Cargar sabores y productos
 // COMPLETEN USTEDES
-let sabores;
-let productos;
+let sabores = JSON.parse(fs.readFileSync("data/sabores.json"));
+let productos = JSON.parse(fs.readFileSync("data/productos.json"));
 
 // Ingresar cliente
 let cliente = ingresarCliente();
@@ -21,3 +22,13 @@ let saboresElegidos = seleccionarSabores(sabores, gustos);
 
 // Guardar pedido
 // COMPLETEN USTEDES
+
+let datos = JSON.parse(fs.readFileSync("data/pedidos.json", "utf-8"))
+datos.push(cliente)
+datos.push(producto)
+datos.push(saboresElegidos)
+let nuevojson = JSON.stringify(datos)
+
+fs.writeFileSync("data/pedidos.json", nuevojson)
+
+// JSON es como una forma de datos que es comun en muchos lenguajes de programacion, entonces para pasar datos lo tenes que hacer en json. Este TP consistia en agarrar unos datos de una funcion que nos daban, y guardarlos en un archivo usando JSON y lectura y escritura de archivos
